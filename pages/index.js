132dc5126d786857c315a69e581036f4c0d63e13
Charles Bergeron <chuck.bergeron@gmail.com>
2020-05-22 10:31:57 -0700

    * initial import

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,102 @@
-import Head from 'next/head'
+import React, { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+import PoolV2ABI from '@pooltogether/pooltogether-contracts/abis/BasePool.json'
+
+const pt = require('pooltogetherjs')
+
+let provider = ethers.getDefaultProvider()
+
+// Mainnet Dai Pool v2 Contract:
+const contractAddress = '0x29fe7D60DdF151E5b52e5FAB4f1325da6b2bD958'
+
+// v2 Pool Contract ABI from '@pooltogether/pooltogether-contracts':
+const abi = PoolV2ABI
+
+const contract = new ethers.Contract(
+  contractAddress,
+  abi,
+  provider
+)
+
+const contractDataEffect = async () => {
+  const accountedBalance = await contract.accountedBalance()
+
+  // Balance is a non-constant function (more on this here: https://docs.ethers.io/ethers.js/html/api-contract.html)
+  // so we need to craft the call data manually
+  const balanceCallData = contract.interface.functions.balance.encode([])
+  const result = await provider.call({ to: contract.address, data: balanceCallData })
+  const balance = contract.interface.functions.balance.decode(result)
+
+  // We need the current draw ID to get the current Draw data
+  const currentOpenDrawId = await contract.currentOpenDrawId()
+  const currentDraw = await contract.getDraw(currentOpenDrawId)
+
+  return {
+    balance,
+    accountedBalance,
+    currentDraw,
+  }
+}
 
 export default function Home() {
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await contractDataEffect()
+      setData(data)
+    }
+    getData()
+  }, [])
+
+  const {
+    balance,
+    accountedBalance,
+    currentDraw,
+  } = data
+
+  let prize = ethers.utils.bigNumberify(0)
+  if (balance) {
+    prize = pt.utils.calculatePrize(
+      balance,
+      accountedBalance,
+      currentDraw.feeFraction
+    )
+  }
+
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div
+      className="container"
+      style={{
+        background: 'rgb(23, 10, 40)',
+        color: '#fff',
+        padding: 44,
+        fontFamily: 'Arial',
+      }}
+    >
+      <div
+        style={{
+          fontSize: 40
+        }}
+      >
+        Prize in DAI:
+        <br />
+        {ethers.utils.formatUnits(prize.toString(), 18)}  
+      </div>
+      
+      <br />
+      <br />
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <div
+        style={{
+          color: '#5ac',
+          fontSize: 20
+        }}
+      >
+        Prize in Wei:
+        <br />
+        {prize.toString()}
+      </div>
     </div>
   )
 }
